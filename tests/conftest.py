2daa7cae3ba5ff96dbded77d1b721d0581f3c2fe
manishvenu <manish.venumuddula@gmail.com>
2024-10-21 10:53:35 -0600

    Additional Tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,3 @@
 import pytest
 
 
-@pytest.fixture
-def temp_dir():
-    return "tests/temp"
--- tests/test_installation.py
+++ tests/test_installation.py
@@ -1,9 +1,30 @@
 import crocodileregionalruckus as crr
+from crocodileregionalruckus import grid_gen
 from crocodileregionalruckus.rm6 import regional_mom6 as rm6
 import pytest
 
 
-def test_import(temp_dir):
+def test_crr_import(tmp_path):
+    """
+    This test confirms we can import crr driver, and generate a crr_driver object which includes grid_gen, regional_mom6, and regional_casegen objects.
+    """
 
     crr_driver_obj = crr.driver.crr_driver()
-    crr_driver_obj.setup_directories(mom_input_dir=temp_dir+"/mom_input", mom_run_dir=temp_dir+"/mom_run")
+    crr_driver_obj.setup_directories(mom_input_dir=tmp_path/"mom_input", mom_run_dir=tmp_path/"mom_run")
+    assert True
+
+def test_rm6_import():
+    """
+    This test confirms we can import rm6, and can call the functions inside
+    """
+    
+    empty_expt_obj = rm6.experiment.create_empty()
+    assert empty_expt_obj is not None
+
+def test_grid_gen_import():
+    """
+    This test confirms we can import crr driver, and generate a crr_driver object which includes grid_gen, regional_mom6, and regional_casegen objects.
+    """
+
+    grid_gen_obj = grid_gen.GridGen()
+    assert grid_gen_obj is not None
