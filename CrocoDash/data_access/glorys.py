562def9db879c0cc31f00ff57614d47762037b6e
Aidan Janney <aidan.janney@colorado.edu>
2025-03-14 14:07:30 -0600

    Working with Manish on DataAccess module, this is some initial work for Glorys Data Access to generalize

--- CrocoDash/data_access/glorys.py
+++ CrocoDash/data_access/glorys.py
@@ -14,7 +14,6 @@ from .utils import setup_logger
 
 logger = setup_logger(__name__)
 
-
 def get_glorys_data_from_rda(
     dates: list,
     lat_min,
@@ -40,6 +39,11 @@ def get_glorys_data_from_rda(
         pattern = os.path.join(ds_in_path, "**", f"*_{date}_*.nc")
         ds_in_files.extend(glob.glob(pattern, recursive=True))
     ds_in_files = sorted(ds_in_files)
+    
+    # Adjust lat lon inputs to make sure they are in the correct range of -180 to 180
+    lat_lon = [lat_min, lat_max, lon_min, lon_max]
+    lat_min, lat_max, lon_min, lon_max = [(num-360) if num > 180 else num for num in lat_lon]
+
     dataset = (
         xr.open_mfdataset(ds_in_files, decode_times=False)
         .drop_vars(drop_var_lst)
@@ -51,6 +55,91 @@ def get_glorys_data_from_rda(
 
     dataset.to_netcdf(path)
     return path
+
+def get_glorys_rda_piecewise(
+    dates: list,
+    lat_min,
+    lat_max,
+    lon_min,
+    lon_max,
+    step_days = None,
+    output_dir=Path(""),
+    output_file="raw_glorys.nc"):
+    """
+    Get GLORYS data in chunks when the time frame is too large.
+    For example, if you set step_days = 5 it will
+    get the data from the given date range 5 days at a time. 
+    
+    This will result in (total # days)/(step_days) number of files (e.g. for 30 days of data with step_size = 5, this would generate 6 files). These six files can then be regridded individually and merged together to avoid memory overuse. 
+    
+    Arguments:
+        - dates (list): two element list of the form [start_date,end_date], date format must be acceptable by pd.date_range
+        - lat_min (float): latitude minimum for your rectangular boundary
+        - lat_max (float): latitude maximum for boundary
+        - lon_min (float): longitude minimum for boundary
+        - lon_max (float): longitude maximum for boundary
+        - step_days (int): number of days each piecewise file should cover at a maximum (some may be shorter if time frame is not divisible by step_days). Default is None, indicating all of the data should be retrieved in one file.
+        - output_dir (string): path where resulting data file(s) should be stored
+        - output_file (string): name (or structure of name) for output files (e.g. output_file="raw_glorys.nc" -> raw_glorys[start_date]_[start_date+step_size].nc etc.)
+        
+        THIS DOC STRING NEEDS A LOT OF WORK, COULD BE WAY LESS RAMBLY, BUT I WANTED TO HAVE SOMETHING
+    """
+    
+    if step_days == None:
+        logger.info(f"Bypassing piecwise retrieval of glorys data. Retrieving entire time frame at once.")
+        path = get_glorys_data_from_rda(
+            dates = dates, 
+            lat_min = lat_min,
+            lat_max = lat_max,
+            lon_min = lon_min,
+            lon_max = lon_max,
+            output_dir = output_dir,
+            output_file = output_file,
+            )
+        return path
+    
+    if step_days <= 0:
+        raise ValueError("step_days must be a positive integer or a None type")
+    
+    dates = pd.date_range(start=dates[0], end=dates[1]).to_pydatetime().tolist()
+    print("TODO - Look into pd.date_range freq argument, does it catch remainder??")
+    date_strings = [date.strftime("%Y%m%d") for date in dates]
+    
+    num_files = (len(dates)+step_days-1)//step_days # ceiling division of number of days with step_days
+    
+    path = Path(output_dir) / output_file
+    
+    logger.info(f"Downloading Glorys Data from RDA from {dates[0]} to {dates[-1]}.")
+    logger.info(f"This will result in {num_files} total files.")
+    
+    ## Grab data step_days number of days at a time
+    # Grab step_days sized windows from the dates
+    date_ranges = [[date_strings[i], date_strings[i+4]] for i in range(0,len(date_strings)-step_days,step_days)]
+    # Get any remainder
+    remaining_days = len(dates)%step_days
+    if (remaining_days != 0):
+        date_ranges.append([date_strings[-remaining_days],date_strings[-1]])
+        
+    # DEV: Check that it returns the expected number of date ranges
+    assert(len(date_ranges)==num_files),"DEV_ERROR: Error with date_ranges in get_glorys_rda_piecewise"
+    
+    # Now actually getting the data!
+    format_output = output_file.rsplit('.',1)
+    assert(len(format_output) == 2), "Please pass in an outputfile name with a valid file type ending (e.g. file.nc)"
+    
+    for date_range in date_ranges:
+        adjusted_output = f"{format_output[0]}.{date_range[0]}_{date_range[1]}.{format_output[1]}"
+        path = get_glorys_data_from_rda(
+            dates = date_range, 
+            lat_min = lat_min,
+            lat_max = lat_max,
+            lon_min = lon_min,
+            lon_max = lon_max,
+            output_dir = output_dir,
+            output_file = adjusted_output,
+            )
+        
+    logger.info(f"Successfull retrieved (probably, if there were no angry red errors) Glorys Data from RDA located in {output_dir} directory.")
 
 
 def get_glorys_data_from_cds_api(
--- CrocoDash/topo.py
+++ CrocoDash/topo.py
@@ -2,6 +2,7 @@ import xarray as xr
 
 from mom6_bathy.topo import Topo as mom6_bathy_Topo
 import regional_mom6 as rmom6
+from pathlib import Path
 
 
 class Topo(mom6_bathy_Topo):
@@ -44,3 +45,47 @@ class Topo(mom6_bathy_Topo):
             positive_down=positive_down,
             write_to_file=False,
         ).depth
+        
+    def setup_bathymetry_mpi(
+        self,
+        *,
+        file_path,
+        longitude_coordinate_name,
+        latitude_coordinate_name,
+        vertical_coordinate_name,
+        temp_bathy_directory,
+        fill_channels=False,
+        positive_down=False
+    ):
+
+        expt = rmom6.experiment.create_empty()
+
+        # Note: What regional_mom6 calls the hgrid is actually the supergrid
+        class HGrid:
+            pass
+
+        expt.hgrid = HGrid()
+        expt.hgrid.x = xr.DataArray(self._grid._supergrid.x, dims=("nyp", "nxp"))
+        expt.hgrid.y = xr.DataArray(self._grid._supergrid.y, dims=("nyp", "nxp"))
+        expt.latitude_extent = [
+            self._grid._supergrid.y.min(),
+            self._grid._supergrid.y.max(),
+        ]
+        expt.longitude_extent = [
+            self._grid._supergrid.x.min(),
+            self._grid._supergrid.x.max(),
+        ]
+        expt.mom_input_dir=Path(temp_bathy_directory)
+
+        expt.setup_bathymetry(
+            bathymetry_path=file_path,
+            longitude_coordinate_name=longitude_coordinate_name,
+            latitude_coordinate_name=latitude_coordinate_name,
+            vertical_coordinate_name=vertical_coordinate_name,
+            fill_channels=fill_channels,
+            positive_down=positive_down,
+            write_to_file=True,
+            only_setup_bathy=True,
+        )
+        
+        return expt
