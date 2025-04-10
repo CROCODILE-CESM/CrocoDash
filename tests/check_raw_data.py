771624f622ec77503614adb07897073ee1eaf200
manishvenu <manish.venumuddula@gmail.com>
2025-04-10 09:56:59 -0600

    Random Stuff

--- tests/check_raw_data.py
+++ tests/check_raw_data.py
@@ -4,10 +4,17 @@ from pathlib import Path
 CSV_FILE = Path(__file__).parent.parent / "CrocoDash" /"raw_data_access"/"tables"/ "data_product_registry.csv"
 
 def main():
- with open(CSV_FILE, newline='', encoding='utf-8') as file:
+    results = {}
+    with open(CSV_FILE, newline='', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
             dataset_name = row["Product_Name"]
-            print(dataset_name)
+            results[dataset_name] = {
+                "links":{},
+                "module":{}
+            }
+            links = row["Links"]
+            
+
 if __name__ == "__main__":
     main()
