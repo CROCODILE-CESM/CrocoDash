f7652f373998ff1ccd73b9f55f1b65d351cdbaea
manishvenu <manish.venumuddula@gmail.com>
2025-06-18 10:12:19 -0600

    Logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,4 +40,4 @@ exclude_patterns = []
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 nbsphinx_execute="never"
-html_logo = "_static/logo.png"
+html_logo = "_static/crocodile.png"
