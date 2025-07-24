31a5b1b48817dbbdd8f5b6e0a6a0d588663c9427
Manish Venumuddula <80477243+manishvenu@users.noreply.github.com>
2025-07-24 17:12:03 -0600

    Add CHL Global Processing (#64)
    * SeaWifs Data Access
    
    * Black
    
    * Seawifs Processed Func
    
    * Black
    
    * Bleh
    
    * Fix Script
    
    * RM6 Commit
    
    * Blank
    
    * Remove demos
    
    * Final Debug

--- tests/raw_data_access/test_seawifs.py
+++ tests/raw_data_access/test_seawifs.py
@@ -7,3 +7,12 @@ def test_get_global_seawifs_script_for_cli(tmp_path):
     path = sw.get_global_seawifs_script_for_cli(output_dir=tmp_path, username="test")
 
     assert os.path.exists(path)
+
+
+def test_get_processed_global_seawifs_script_for_cli(tmp_path):
+
+    path = sw.get_processed_global_seawifs_script_for_cli(
+        output_dir=tmp_path, output_file="test.nc"
+    )
+
+    assert os.path.exists(path)
