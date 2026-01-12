af53089fb53aa9bb9c7040b60b44500b88587847
manishvenu <manish.venumuddula@gmail.com>
2026-01-12 11:22:54 -0700

    Bleh

--- tests/extract_forcings/test_other_process_forcings.py
+++ tests/extract_forcings/test_other_process_forcings.py
@@ -7,12 +7,4 @@ from CrocoDash.extract_forcings import (
 
 
 def test_process_chl():
-    raise NotADirectoryError(
-        "Chlorophyll processing is not implemented yet. It's tricky because we need to setup the regional_mom6 experiment object first."
-    )
-    chl.process_chl(
-        ocn_grid=self.ocn_grid,
-        ocn_topo=self.ocn_topo,
-        chl_processed_filepath=self.fcr["chl"].chl_processed_filepath,
-        output_filepath=self.fcr["chl"].chl_output_filepath,
-    )
+    pass
