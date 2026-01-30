f7652d3e635546a9e8480d05220fdd96d9037aad
manishvenu <manish.venumuddula@gmail.com>
2026-01-30 16:03:11 -0700

    Black

--- tests/shareable/test_identify.py
+++ tests/shareable/test_identify.py
@@ -1,7 +1,14 @@
 from CrocoDash.shareable.identify import *
 
+
 def test_diff_CESM_cases(skip_if_not_glade):
-    print(diff_CESM_cases("/glade/u/home/manishrv/croc_cases/smoke.sink.cesm.1", "/glade/u/home/manishrv/croc_cases/smoke.sink.bgc.cesm.1"))
+    print(
+        diff_CESM_cases(
+            "/glade/u/home/manishrv/croc_cases/smoke.sink.cesm.1",
+            "/glade/u/home/manishrv/croc_cases/smoke.sink.bgc.cesm.1",
+        )
+    )
+
 
 if __name__ == "__main__":
     test_diff_CESM_cases("Ntohginwsodf")
