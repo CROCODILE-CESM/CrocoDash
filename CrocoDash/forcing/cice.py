af9edcc23e8ed4085ae53a324c1953bc87c79bcc
manishvenu <manish.venumuddula@gmail.com>
2026-09-25 15:38:45 -0600

    Last Dave Change

--- CrocoDash/forcing/cice.py
+++ CrocoDash/forcing/cice.py
@@ -390,7 +390,7 @@ class CICEConfigurator(BaseConfigurator):
             self.set_output_param("restore_width", 1)
             self.set_output_param("restore_data", "'initial'")
             self.set_output_param(
-                "restore_flds", "'aicen','vicen','vsnon','trcrn','velocity'"
+                "restore_flds", "'state'"
             )
 
         self.set_output_param("histfreq", "'m','d','x','x','x'")
